b233750e3c8f862d88363fd63d3adcec1bec3a64
shalomprasannan <34344441+shalomprasannan@users.noreply.github.com>
2023-04-21 12:34:53 +0530

    Update ServiceChecker.ps1
    Styled HTML Page with Passed and Failed

--- ServiceChecker.ps1
+++ ServiceChecker.ps1
@@ -9,38 +9,116 @@ function ServiceCheck {
 
     # Get the columns from the reference CSV file
     $columns = $reference[0].PSObject.Properties.Name
-
+    #$credentials=get-
     # Get the services from the remote computer
-    $services = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-        Get-Service | Select-Object $columns
-    }
+    #$services = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        $result=Get-Service | Select-Object $columns
+    #}
 
     # Compare the reference and services
-    $result = Compare-Object $reference $services -Property $columns
+    $Compared = Compare-Object $reference $services -Property $columns -IncludeEqual
+    write-host "ran"
+    $processed=foreach($row in $compared){
+        #looping only on the equal and reference data == & <=
+        #reset all variable
+        $HTML_verbose=""
+        $changedProperty=@()
+        if($row.sideIndicator -ne "=>"){
+            $result_row=$result|where {$_.Name -eq $row.name}
+            if($row.sideIndicator -eq "<="){
 
-    # Generate the output objects
-    $differences = $result | Where-Object { $_.SideIndicator -eq "<=" }
-    $output = foreach ($diff in $differences) {
-        $referenceObj = $reference | Where-Object { $_.Name -eq $diff.Name }
-        $servicesObj = $services | Where-Object { $_.Name -eq $diff.Name }
-        if ($servicesobj) {
-            $changedProperty = foreach ($prop in $referenceObj.psobject.properties.name) {
-                if ($referenceObj.$prop -ne $servicesObj.$prop) {
-                    $prop
+                if($result_row){
+                    foreach ($column in $columns) {
+                        if ($row.$column -ne $result_row.$column) {
+                            $changedProperty+=$column
+                            $HTML_verbose="$column : expected <b>$($row.$column)</b> got <b>$($result_row.$column)</b>"
+                        }
+                    }
                 }
+                else{
+                    $changedProperty="Doesn't exist"
+                    $HTML_verbose="Name Doesn't exist"
+                }
+                $status="Failed"
+                $HTML_status="<span style=`"color:Tomato;`">Failed</span>"
             }
-        } else {
-            $changedProperty = "Name"
-        }
+            elseif($row.sideIndicator -eq "=="){
+                $HTML_status="<span style=`"color:Green;`">Passed</span>"
+                $status="Passed"
+            }
 
-        # Add the columns from the reference CSV file to the output object
-        $props = @{}
-        foreach ($column in $columns) {
-            $props[$column] = $diff.$column
+            $props = @{}
+
+            #add the reference columns
+            foreach ($column in $columns) {
+                $props[$column] = $row.$column
+            }
+
+            #add the differencing column from result
+            $props.Result=$result_row.StartType 
+            
+            #add some default additional columns
+            $props.Status = $status
+            $props.HTML_status=$HTML_status
+            $props.HTML_Verbose=$HTML_verbose
+            $props.Server=$ComputerName
+            $props.Changed="$changedProperty"
+            
+            
+            [PSCustomObject]$props
+            
+            <#[PSCustomObject]@{
+                Name=$row.Name
+                StartType=$row.StartType
+                Result=$result_row.StartType
+                Status = $status
+                Verbose=$verbose
+                Server=$ComputerName
+            }#>
         }
-        $props.ChangedProperty = $changedProperty
-        [PSCustomObject]$props
     }
+    $Html= $processed|select Server, Name, StartType, Result,@{l="Status";e={$_.HTML_Status}}, @{l="Verbose";e={$_.HTML_Verbose}} |sort Name|ConvertTo-Html
+    $processed|select Name, changed
+    $styledHTML=@"
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table {
+      font-family: Arial, Helvetica, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    td, th {
+      border: 1px solid #ddd;
+      padding: 8px;
+    }
+    
+    tr:nth-child(even){background-color: #f2f2f2;}
+    
+    tr:hover {background-color: #ddd;}
+    
+    th {
+      padding-top: 12px;
+      padding-bottom: 12px;
+      text-align: left;
+      background-color: #04AA6D;
+      color: white;
+    }
+    </style>
+    </head>
+    <body>
+    $Html
+    </body>
+    </html>
+"@
+    $decoded=[System.Web.HttpUtility]::HtmlDecode($StyledHTML)
 
-    $output|select Name, startType, ChangedProperty, @{l="ServerName";e={"$computername"}}, @{l="status";e={"Failed"}}
+    $decoded|Out-File .\a.html
+    #$output
+    #$result|select Name, startType, sideIndicator, @{l="ServerName";e={"$computername"}} | format-table
+
 }
+
+ServiceCheck
