e2184706e994591b741224bc64006b2a4a28f8dd
shalomprasannan <34344441+shalomprasannan@users.noreply.github.com>
2023-04-24 10:25:29 +0530

    Update Functions.ps1
    All-ServerCheck -os null -env null -computerName xavda06.m1.local -tests Roles
    
    -tests can have below parameters Agents, Services, Roles, Registry, All
    -os and -env are commented out for testing purposes

--- Functions.ps1
+++ Functions.ps1
@@ -18,6 +18,7 @@ function GetRemoteRegistry {
 
 #function to get result Registry object
 function GetRegistry {
+    write-host "Fetching Registries..."
     $folder=".\references\"
     $reference=import-csv "$folder\registry.csv"
     $resultregistry=@()
@@ -30,6 +31,7 @@ function GetRegistry {
 
 #function to get installed Agents
 function GetAgents {
+    write-host "Fetching Installed Agents..."
     $resultAgents=Invoke-command -Session $session -ScriptBlock{
     $programs = Get-cimInstance -Classname Win32_Product | Select Name, Version
     $programs}
@@ -38,6 +40,7 @@ function GetAgents {
 
 #function to get Services
 function getServices {
+    write-host "Fetching Installed Services..."
     $services=Invoke-command -Session $session -ScriptBlock{
         Get-Service | Select Name, @{Name='StartType';Expression={
         switch ($_.StartType) {'Automatic' { 'Automatic' }
@@ -49,6 +52,7 @@ function getServices {
 
 #function to get Roles
 function getRoles{
+    write-host "Fetching Installed Roles..."
     $roles=Invoke-command -Session $session -ScriptBlock{
         Get-WindowsFeature | Select Name,Installed 
     }
@@ -65,6 +69,7 @@ function Result-object{
         [Parameter(Mandatory=$true)]
         [string[]]$columns
     )
+    write-host "Comparing Reference $test with fetched $test"
     $Compared = Compare-Object $reference $result -Property $columns -IncludeEqual
     $compared = $compared|where {$_.sideindicator -ne "=>"}
     foreach($compObj in $compared){
@@ -94,14 +99,33 @@ function Result-object{
 function HTMLMaker{
     param(
        [Parameter(Mandatory=$true)]
-       [object]$fragment
+       [object]$object,
+       [Parameter(Mandatory=$true)]
+       [object]$columns,
+       [Parameter(Mandatory=$true)]
+       [object]$test
     )
+    write-host "Exporting $test HTML`n"
+
+    #aggregating the status
+    $counts=$object|group-object status
+    $passed=$counts[0].count
+    $failed=$counts[1].count
+    $total=$passed+$failed
+
+    $fragment = $object|select $($columns+$HTMLColumns)|convertto-HTML -fragment
     $html=@"
     <!DOCTYPE html>
     <html>
     <head>
     <title>$test - $computername</title>
     <style>
+    body {
+			margin: 0;
+			padding: 0;
+			background-color: #f5f5f5;
+		}
+
     table {
       font-family: Arial, Helvetica, sans-serif;
       border-collapse: collapse;
@@ -126,12 +150,39 @@ function HTMLMaker{
       background-color: #04AA6D;
       color: white;
     }
+
+    .status-bar {
+			position: fixed;
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			height: 40px;
+			background-color: #333;
+			color: #888;
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+			font-size: 14px;
+			font-weight: bold;
+			letter-spacing: 1px;
+		}
+    .white{
+        color:white;
+    }
+
     </style>
     </head>
-    <body>
-    <p>Report last ran at $(get-date) </p>
-    $fragment
-    </body>
+        <body>
+        $fragment
+            <div class="status-bar">
+                <div>
+		            <p>Report last ran at <span class="white">$(get-date -Format "dd-mm-yyyy HH:mm")</span></p>
+                </div>
+                <div>
+                    <p><span class="white">$failed </span> failed out of <span class="white"> $total</span></p>
+                </div>
+	        </div>
+        </body>
     </html>
 "@
     [System.Net.WebUtility]::HtmlDecode($HTML)
@@ -144,56 +195,75 @@ function HTMLMaker{
 function All-ServerCheck{
     Param(
         [Parameter(Mandatory=$true)]
-        [string]$computerName
+        [string]$computerName,
+        [Parameter(Mandatory=$true)]
+        [string[]]$os,
+        [Parameter(Mandatory=$true)]
+        [string[]]$env,
+        [Parameter(Mandatory=$true)]
+        [string[]]$tests
         )
     $global:computername=$computername
-    $global:SourceFolder="$($pwd.Path)\References\"
-    $global:OutputFolder="$($pwd.Path)\Output\"
+    $global:SourceFolder="$($pwd.Path)\References\"#$os\$env"
+
+    $global:OutputFolder="$($pwd.Path)\Output\$computername\"
+    if (-not (test-path $outputfolder)){
+        New-Item -ItemType Directory -Path $outputfolder
+    }
     $global:Session=new-pssession -ComputerName $computername
 
-    <#
+    
     $global:csvColumns="Result","Status"
-    $global:HTMLColumns="Result","HTML_Status","HTML_Verbose"
+    $global:HTMLColumns="Result",@{Name='Status';Expression={$_.HTML_Status}},@{Name='Verbose';Expression={$_.HTML_Verbose}}
     $global:agentsColumns="Name","Version"
     $global:ServicesColumns="Name","StartType"
     $global:RolesColumns="Name","Installed"
     $global:RegistryColumns="Path","Key","Value"
-    #>
 
+
+    $agentsTest={
     $global:Agents=GetAgents
     $global:RefAgents=Import-csv "$SourceFolder\Agents.csv"
     $agentsResult=result-object -reference $RefAgents -result $Agents -columns Name,Version|Sort-Object Name
-    $agentsResult|select Name,Version,Result,status,verbose|export-csv -NoTypeInformation "$outputFolder\Agents.csv"
-    $AgentsFragment = $AgentsResult|select Name,Version,Result,HTML_Status,HTML_Verbose|convertto-HTML -fragment
-    HTMLMaker $AgentsFragment |out-file "$outputfolder\Agents.html"
+    $agentsResult|select @($AgentsColumns + $csvColumns)|export-csv -NoTypeInformation "$outputFolder\Agents.csv"
+    HTMLMaker $AgentsResult $AgentsColumns $test |out-file "$outputfolder\Agents.html"
+    }
 
-
+    $ServicesTest={
     $global:Services=getServices
     $global:RefServices=Import-csv "$SourceFolder\Services.csv"
     $ServicesResult=result-object -reference $RefServices -result $services -columns Name,StartType|Sort-Object Name
-    $ServicesResult|select Name,StartType,Result,status,verbose|export-csv -NoTypeInformation "$outputFolder\Services.csv"
-    $ServicesFragment = $ServicesResult|select Name,StartType,Result,HTML_Status,HTML_Verbose|convertto-HTML -fragment
-    HTMLMaker $ServicesFragment  |out-file "$outputfolder\Services.html"
-
+    $ServicesResult|select @($ServicesColumns + $csvColumns)|export-csv -NoTypeInformation "$outputFolder\Services.csv"
+    HTMLMaker $ServicesResult $ServicesColumns $test  |out-file "$outputfolder\Services.html"
+    }
     
+    $RolesTest={
     $global:Roles=getRoles
     $global:RefRoles=Import-csv "$SourceFolder\Roles.csv"
     $RolesResult=result-object -reference $RefRoles -result $roles -columns Name,Installed|Sort-Object Name
-    $RolesResult|select Name,Installed,Result,status,verbose|export-csv -NoTypeInformation "$outputFolder\Roles.csv"
-    $RolesFragment = $RolesResult|select Name,Installed,Result,HTML_Status,HTML_Verbose|convertto-HTML -fragment
-    HTMLMaker $RolesFragment |out-file "$outputfolder\Roles.html"
-    
+    $RolesResult|select @($RolesColumns + $csvColumns)|export-csv -NoTypeInformation "$outputFolder\Roles.csv"
+    HTMLMaker $RolesResult $RolesColumns $test |out-file "$outputfolder\Roles.html"
+    }
+
+    $RegistryTest={
     $global:Registry=getRegistry
     $global:RefRegistry=Import-csv "$SourceFolder\Registry.csv"
     $RegistryResult=result-object -reference $RefRegistry -result $Registry -columns Key,value|Sort-Object Path,Key
-    $RegistryResult|select Path,Key, Value,Result,status,verbose|export-csv -NoTypeInformation "$outputFolder\Registry.csv"
-    $registryFragment = $RegistryResult|select Path,Key,Value,Result,HTML_Status,HTML_Verbose|convertto-HTML -fragment
-    HTMLMaker $RegistryFragment |out-file "$outputfolder\Registry.html"
-    
-    <#
-    $agentsFragment=$agentsresult|convertto-html -Fragment
-    $ServicesFragment=$Servicesresult|convertto-html -Fragment
-    $RolesFragment=$Rolesresult|convertto-html -Fragment
-    $RegistryFragment=$Registryresult|convertto-html -Fragment
-    #>
+    $RegistryResult|select @($RegistryColumns + $csvColumns)|export-csv -NoTypeInformation "$outputFolder\Registry.csv"
+    HTMLMaker $RegistryResult $registryColumns $test |out-file "$outputfolder\Registry.html"
+    }
+
+    if($tests -eq "All"){
+    $tests=@("Agents","Services","Roles","Registry")
+    }
+
+    foreach($test in $tests){
+        switch ($test) {
+            'Agents'   { &$agentsTest }
+            'Services' { &$servicesTest }
+            'Roles'    { &$rolesTest }
+            'Registry' { &$registryTest }
+            Default    { Write-Host "Invalid test name" }
+        }
+    }
 }
